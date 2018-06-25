1be85b2662cd6d6285d565ab06b9c118a133002a
Esraa Hero <esraa.abdelaziz141@gmail.com>
2018-06-25 05:25:39 +0200

    fetching modification in readme

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@ var Enemy = function (x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    this.imageWidth = 70;
-    this.imageHeight = 90;
+    this.imageWidth = 101;
+    this.imageHeight = 171;
     this.speed = speed;
 };
 
@@ -27,15 +27,15 @@ Enemy.prototype.update = function (dt) {
     }
     if (Math.abs(Math.floor(player.x) - Math.floor(this.x)) <= 50 &&
         Math.abs(Math.floor(player.y) - Math.floor(this.y)) <= 20) {
-        player.x = 200;
-        player.y = 300;
+        player.x = Math.random() * 500;
+        player.y = 400;
     }
 
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.imageWidth, this.imageHeight);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
@@ -73,7 +73,7 @@ class Player {
             //the close icon in  model
             const close = document.querySelector('.close');
             this.x = 200;
-            this.y = 300;
+            this.y = 400;
             //reseting speed to zero
             allEnemies.forEach(function (element) {
                 element.speed = 0;
@@ -98,16 +98,16 @@ class Player {
     handleInput(key) {
         switch (key) {
             case 'left':
-                this.update(this.x -= 50);
+                this.update(this.x -= 101);
                 break;
             case 'right':
-                this.update(this.x += 50);
+                this.update(this.x += 101);
                 break;
             case 'up':
-                this.update(this.y -= 50);
+                this.update(this.y -= 83);
                 break;
             case 'down':
-                this.update(this.y += 50);
+                this.update(this.y += 83);
 
         }
 
@@ -118,17 +118,11 @@ class Player {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
-const allEnemies = [];
-for (i = 0; i <= 2; i++) {
-    let xDirectION = Math.floor(Math.random() * 20);
-    let YDirection = 100 + 80 * i;
-    let speed = Math.floor(Math.random() * 70 + 100);
-    allEnemies.push(new Enemy(xDirectION, YDirection, speed))
-}
-
-const player = new Player(200, 300);
+const allEnemies = [new Enemy(20, 60, Math.floor(Math.random() * 70 + 100)),
+    new Enemy(120, 150, Math.floor(Math.random() * 70 + 100)),
+    new Enemy(220, 230, Math.floor(Math.random() * 70 + 100))
+];
+const player = new Player(200, 400);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function (e) {
